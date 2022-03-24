54bdc88459b2bcb807ebe36ba9a8bd5c94043364
vlomyy <provorov.012@mail.ru>
2022-03-24 14:17:34 +0300

    33

--- meth4/Program.cs
+++ meth4/Program.cs
@@ -21,6 +21,7 @@ for (int i= 0; i<mas.Length;i++)
     if (mas[i] > srzn) uebs++;
 }
 
+Console.WriteLine("Сейчас будет ответ");
 Console.WriteLine("Количество уебансов" + uebs);
 
 static int srznachelem(int[] arr)
